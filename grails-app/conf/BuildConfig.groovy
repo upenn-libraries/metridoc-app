2a099ba1442b159726a0fe783427efd92fac6e13
Tommy Barker <mingus.karate@gmail.com>
2013-09-17 16:06:40 -0400

    Update BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,7 +32,7 @@ grails.project.dependency.resolution = {
         compile (":metridoc-counter:0.3") {
             excludes "metridoc-core"
         }
-        compile (":metridoc-illiad:0.4.1") {
+        compile (":metridoc-illiad:0.4.2") {
             excludes "metridoc-core"
         }
         compile (":metridoc-rid:0.3.3") {
