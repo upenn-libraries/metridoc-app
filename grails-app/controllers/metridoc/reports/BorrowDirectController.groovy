de498d407fa93386ae19c63b13d086e1e29069b2
Tommy Barker <mingus.karate@gmail.com>
2013-10-17 09:59:19 -0400

    add redirects
    add redirects to BorrowDirect, EzBorrow and Funds

---
+++ grails-app/controllers/metridoc/reports/BorrowDirectController.groovy
@@ -0,0 +1,17 @@
+package metridoc.reports
+
+class BorrowDirectController {
+
+    static reportName = "Borrow Direct Data Repository"
+
+    static homePage = [
+            title: "Borrow Direct Data Repository",
+            description: """
+                ILL activity within the Borrow Direct consortium.
+            """
+    ]
+
+    def index() {
+        redirect(url: "https://metridoc.library.upenn.edu/metridoc-penn-borrow/borrowDirect")
+    }
+}
---
+++ grails-app/controllers/metridoc/reports/EzBorrowController.groovy
@@ -0,0 +1,17 @@
+package metridoc.reports
+
+class EzBorrowController {
+
+    static reportName = "EzBorrow Data Repository"
+
+    static homePage = [
+            title: "EzBorrow Data Repository",
+            description: """
+                ILL activity within the EZBorrow consortium.
+            """
+    ]
+
+    def index() {
+        redirect(url: "https://metridoc.library.upenn.edu/metridoc-penn-borrow/EZBorrow")
+    }
+}
